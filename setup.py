975803984fd8a313fc2b48133f6393c1f54783df
Matt McLoughlin <mattmcl@microsoft.com>
2024-10-09 14:57:23 -0700

    torch=2.4.0

--- setup.py
+++ setup.py
@@ -4,24 +4,6 @@ import sys
 with open("README.md", "r") as f:
     readme = f.read()
 
-def get_url() -> str:
-    if sys.version_info[:2] == (3, 8):
-        _ver = "cp38"
-    elif sys.version_info[:2] == (3, 9):
-        _ver = "cp39"
-    elif sys.version_info[:2] == (3, 10):
-        _ver = "cp310"
-    else:
-        raise Exception(f"Python {sys.version} is not supported.")
-
-    # FIXME: how to download on macox??
-    if sys.platform == "win32":
-        _os = "win_amd64"
-    else:
-        _os = "linux_x86_64"
-
-    return f"https://download.pytorch.org/whl/cu113/torch-1.12.0%2Bcu113-{_ver}-{_ver}-{_os}.whl"
-
 setup(
     name="OmegaFold",
     description="OmegaFold Release Code",
@@ -32,7 +14,7 @@ setup(
     entry_points={"console_scripts": ["omegafold=omegafold.__main__:main",],},
     install_requires=[
         "biopython",
-        "torch>=1.12.0"
+        "torch=2.4.0"
     ],
     python_requires=">=3.8",
 )
